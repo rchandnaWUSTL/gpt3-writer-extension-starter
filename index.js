a919764e1460ce471be1941af2940f3a59af7deb
rchandnaWUSTL <45045902+rchandnaWUSTL@users.noreply.github.com>
2023-01-26 01:01:42 -0800

    API call works

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const checkForKey = () => {
     });
 };
 
-document.getElementById('save_key_button').addEventListener('click', saveKey);
-document
-    .getElementById('change_key_button')
-    .addEventListener('click', changeKey);
-
 const encode = (input) => {
     return btoa(input);
 };
